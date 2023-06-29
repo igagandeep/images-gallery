25be7536d14f36542c51de1a8c5c90fba6d7dc41
Gagandeep <igagandeep95@gmail.com>
2023-06-29 17:01:58 -0400

    added modal on images

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,9 +1,39 @@
+import { useState } from "react";
+
 const Images = ({ image }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalImageSrc, setModalImageSrc] = useState("");
+
+    const openModal = (src) => {
+        setIsModalOpen(true);
+        setModalImageSrc(src);
+    };
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setModalImageSrc("");
+    };
+
     return (
         <>
             <li>
-                <img src={image.urls.regular} alt={image.alt_description} />
+                <img
+                    src={image.urls.regular}
+                    alt={image.alt_description}
+                    onClick={() => openModal(image.urls.regular)}
+                />
             </li>
+
+            {isModalOpen && (
+                <div className="modal" onClick={closeModal}>
+                    <span className="close">&times;</span>
+                    <img
+                        className="modal-content"
+                        src={modalImageSrc}
+                        alt="this is the modal pic"
+                        onClick={(e) => e.stopPropagation()}
+                    />
+                </div>
+            )}
         </>
     );
 };
