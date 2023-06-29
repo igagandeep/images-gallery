2b92d26eb281ac9f9d9d4d6538b8dae2589a88f6
Gagandeep <igagandeep95@gmail.com>
2023-06-29 18:18:24 -0400

    added likes download buttons

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { BsFillHeartFill, BsPlus, BsDownload } from "react-icons/bs";
 
 const Images = ({ image }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [modalImageSrc, setModalImageSrc] = useState("");
+    const [isHovered, setIsHovered] = useState(false);
 
     const openModal = (src) => {
         setIsModalOpen(true);
@@ -13,14 +15,46 @@ const Images = ({ image }) => {
         setModalImageSrc("");
     };
 
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
     return (
         <>
-            <li>
+            <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                 <img
                     src={image.urls.regular}
                     alt={image.alt_description}
                     onClick={() => openModal(image.urls.regular)}
                 />
+                {isHovered && (
+                    <>
+                        <div className="top-container">
+                            <button className="like-btn">
+                                <BsFillHeartFill />
+                            </button>
+                            <button className="add-collection">
+                                <BsPlus />
+                            </button>
+                        </div>
+                        <div className="bottom-container">
+                            <div className="profile">
+                                <img
+                                    src={image.user.profile_image.small}
+                                    alt="pic of user who posted the pic"
+                                />
+                                <p>{image.user.first_name}</p>
+                            </div>
+                            <button className="download-btn">
+                                <BsDownload />
+                            </button>
+                        </div>
+                    </>
+                )}
             </li>
 
             {isModalOpen && (
